baa9f94902cf0600021bcc5e678444d5ee77c51c
Alexey Efimov <lex.efimov@swiftbook.ru>
2021-10-04 20:39:25 +0300

    1. Prepare app 2. Setup NB

---
+++ CoreDataDemo/TaskListViewController.swift
@@ -0,0 +1,38 @@
+//
+//  TaskListViewController.swift
+//  CoreDataDemo
+//
+//  Created by Alexey Efimov on 04.10.2021.
+//
+
+import UIKit
+
+class TaskListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        setupNavigationBar()
+    }
+
+    private func setupNavigationBar() {
+        title = "Task List"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let navBarAppearance = UINavigationBarAppearance()
+        
+        navBarAppearance.backgroundColor = UIColor(
+            red: 21/255,
+            green: 101/255,
+            blue: 192/255,
+            alpha: 194/255
+        )
+        
+        navBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+        navBarAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        
+        navigationController?.navigationBar.standardAppearance = navBarAppearance
+        navigationController?.navigationBar.scrollEdgeAppearance = navBarAppearance
+    }
+}
+
